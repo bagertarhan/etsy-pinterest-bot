75c9dbea75fea8f71ca0467e65aca28e3276c7d2
bagertarhan <kskbager1912@gmail.com>
2026-09-03 21:57:23 +0300

    Update pinterest_client.py

--- src/pinterest_client.py
+++ src/pinterest_client.py
@@ -11,7 +11,7 @@ import requests
 
 PINTEREST_AUTH_URL = "https://www.pinterest.com/oauth/"
 PINTEREST_TOKEN_URL = "https://api.pinterest.com/v5/oauth/token"
-PINTEREST_API_BASE = "https://api.pinterest.com/v5"
+PINTEREST_API_BASE = "https://api-sandbox.pinterest.com/v5"
 
 SCOPES = "boards:read,boards:write,pins:read,pins:write"
 
